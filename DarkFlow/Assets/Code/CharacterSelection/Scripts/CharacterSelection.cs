fcc68d59bc4d3dfd5fb84b2600b7274257870f67
GraphicEdit <contact@graphicedit.com>
2023-12-02 12:55:59 +0000

    CharSelect

---
+++ DarkFlow/Assets/Code/CharacterSelection/Scripts/CharacterSelection.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CharacterSelection : MonoBehaviour
+{
+	public GameObject[] characters;
+	public int selectedCharacter = 0;
+
+	public void NextCharacter()
+	{
+		characters[selectedCharacter].SetActive(false);
+		selectedCharacter = (selectedCharacter + 1) % characters.Length;
+		characters[selectedCharacter].SetActive(true);
+	}
+
+	public void PreviousCharacter()
+	{
+		characters[selectedCharacter].SetActive(false);
+		selectedCharacter--;
+		if (selectedCharacter < 0)
+		{
+			selectedCharacter += characters.Length;
+		}
+		characters[selectedCharacter].SetActive(true);
+	}
+
+	public void StartGame()
+	{
+		PlayerPrefs.SetInt("selectedCharacter", selectedCharacter);
+		SceneManager.LoadScene(2, LoadSceneMode.Single);
+	}
+}
