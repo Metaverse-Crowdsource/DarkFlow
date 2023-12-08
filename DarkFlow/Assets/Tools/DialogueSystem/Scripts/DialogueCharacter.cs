29791a14be96f859a08ce9e43b78bfbc751a66fd
GraphicEdit <contact@graphicedit.com>
2023-12-08 10:40:58 +0000

    fix

---
+++ DarkFlow/Assets/Tools/DialogueSystem/Scripts/DialogueCharacter.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+
+    [CreateAssetMenu(fileName = "New Character", menuName = "Dialogue System/New Dialogue Character", order = 1)]
+
+    public class DialogueCharacter : ScriptableObject
+    {
+        public Sprite characterPhoto;
+        public string characterName;
+    }
---
+++ DarkFlow/Assets/Tools/DialogueSystem/Scripts/DialogueTrigger.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+    public class DialogueTrigger : MonoBehaviour
+    {
+        [Header("Events")]
+        public UnityEvent startDialogueEvent;
+        public UnityEvent nextSentenceDialogueEvent;
+        public UnityEvent endDialogueEvent;
+    }
